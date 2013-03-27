0fda107594c0a69ac380b6ba5c3755ca5afc2cb7
EC2 Default User <ec2-user@ip-10-120-38-249.ap-northeast-1.compute.internal>
2013-03-27 07:20:45 +0000

    changed to do beign-rescue

--- adduser/recipes/default.rb
+++ adduser/recipes/default.rb
@@ -7,11 +7,15 @@
 # All rights reserved - Do Not Redistribute
 #
 
-user "matsui" do
-	comment "matsui"
-	uid 1001
-	gid "wheel"
-	home "/home/matsui"
-	shell "/bin/bash"
-	password "$6$cjnsBJ7l$Cx23R0W3ZVxUAtO7E7cx6e.pZi13jxvNfcB4RjtCnSXi.jBV1VhkrNfPhBKQ//r9moVMzhj.EO7ppuRQ0i1VP0"
+begin
+	user "matsui" do
+		comment "matsui"
+		uid 1001
+		gid "wheel"
+		home "/home/matsui"
+		shell "/bin/bash"
+		password "$6$cjnsBJ7l$Cx23R0W3ZVxUAtO7E7cx6e.pZi13jxvNfcB4RjtCnSXi.jBV1VhkrNfPhBKQ//r9moVMzhj.EO7ppuRQ0i1VP0"
+	end
+rescue
+	log "ignore error"
 end
