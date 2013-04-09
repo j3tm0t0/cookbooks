e8717b64bde6fc5e953b7ee91b7ee9a633c17b0c
EC2 Default User <ec2-user@ip-10-121-9-75.ap-northeast-1.compute.internal>
2013-04-09 04:42:09 +0000

    added deploy_hook

---
+++ deploy_hook/recipes/default.rb
@@ -0,0 +1,13 @@
+#
+# Cookbook Name:: deploy_hook
+# Recipe:: default
+#
+# Copyright 2013, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+log "execute deploy_hook"
+
+execute "ps" do
+	command "ps -auxwww > /var/tmp/ps.$$"
+end
